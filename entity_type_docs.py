1fc982e7e9c880329f9dc379cff0c97d5d60afd1
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-03-13 21:14:24 -0400

    Use custom format for entity type docs

--- entity_type_docs.py
+++ entity_type_docs.py
@@ -2,27 +2,82 @@
 from collections import OrderedDict
 
 import os
-
-import yaml
-try:
-  from yaml import CDumper as Dumper
-except ImportError:
-  from yaml import Dumper
-
-# Allow yaml to load and dump OrderedDicts.
-yaml.SafeLoader.add_constructor(
-  yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-  lambda loader, node: OrderedDict(loader.construct_pairs(node))
-)
-yaml.Dumper.add_representer(
-  OrderedDict,
-  lambda dumper, data: dumper.represent_dict(data.items())
-)
+import re
 
 DATA_PATH = "./mclib/data" # TODO
 
 with open(os.path.join(DATA_PATH, "entity_types.txt")) as f:
-  ENTITY_TYPE_DOCS = yaml.safe_load(f)
+  type_doc_str = f.read()
+
+ENTITY_TYPE_DOCS = OrderedDict()
+
+last_seen_type_doc = None
+last_seen_subtype_doc = None
+found_forms_header_for_subtype = False
+for line_index, line in enumerate(type_doc_str.split("\n")):
+  if len(line) == 0:
+    continue
+  if line[0] == "#":
+    continue
+  
+  type_match = re.search(r"^([0-9A-F]{2}) (.+)$", line, re.IGNORECASE)
+  subtype_match = re.search(r"^  ([0-9A-F]{2}) (.+)$", line, re.IGNORECASE)
+  forms_header_match = re.search(r"^    Forms:$", line, re.IGNORECASE)
+  form_match = re.search(r"^      ([0-9A-F]+) (.+)$", line, re.IGNORECASE)
+  if type_match:
+    type = int(type_match.group(1), 16)
+    type_name = type_match.group(2)
+    
+    if type in ENTITY_TYPE_DOCS:
+      raise Exception("Duplicate doc for entity type %X on line %d" % (type, line_index+1))
+    
+    type_doc = OrderedDict()
+    type_doc["name"] = type_name
+    type_doc["subtypes"] = OrderedDict()
+    ENTITY_TYPE_DOCS[type] = type_doc
+    
+    last_seen_type_doc = type_doc
+    last_seen_subtype_doc = None
+    found_forms_header_for_subtype = False
+  elif subtype_match:
+    subtype = int(subtype_match.group(1), 16)
+    subtype_name = subtype_match.group(2)
+    
+    if last_seen_type_doc is None:
+      raise Exception("Found subtype doc not under a type on line %d" % (line_index+1))
+    if subtype in last_seen_type_doc["subtypes"]:
+      raise Exception("Duplicate doc for entity subtype %X on line %d" % (subtype, line_index+1))
+    
+    subtype_doc = OrderedDict()
+    last_seen_type_doc["subtypes"][subtype] = subtype_doc
+    subtype_doc["name"] = subtype_name
+    subtype_doc["forms"] = OrderedDict()
+    
+    last_seen_subtype_doc = subtype_doc
+    found_forms_header_for_subtype = False
+  elif forms_header_match:
+    if last_seen_subtype_doc is None:
+      raise Exception("Found forms list doc not under a subtype on line %d" % (line_index+1))
+    
+    found_forms_header_for_subtype = True
+  elif form_match:
+    form = int(form_match.group(1), 16)
+    form_name = form_match.group(2)
+    
+    if last_seen_subtype_doc is None:
+      raise Exception("Found forms list doc not under a subtype on line %d" % (line_index+1))
+    if not found_forms_header_for_subtype:
+      continue
+    if form in last_seen_subtype_doc["forms"]:
+      raise Exception("Duplicate doc for entity form %X on line %d" % (form, line_index+1))
+    
+    form_doc = OrderedDict()
+    last_seen_subtype_doc["forms"][form] = form_doc
+    form_doc["name"] = form_name
+  else:
+    continue
+
+# TODO: implement best sprite frame into new doc format
 
 class EntityTypeDocs:
   @staticmethod
@@ -39,6 +94,12 @@ class EntityTypeDocs:
       if value in type_data["subtypes"]:
         subtype_data = type_data["subtypes"][value]
         pretty_value += ": " + subtype_data["name"]
+    elif prop.attribute_name == "form" and entity.type in ENTITY_TYPE_DOCS:
+      type_data = ENTITY_TYPE_DOCS[entity.type]
+      if entity.subtype in type_data["subtypes"]:
+        subtype_data = type_data["subtypes"][entity.subtype]
+        if value in subtype_data["forms"]:
+          pretty_value += ": " + subtype_data["forms"][value]["name"]
     
     return pretty_value
   
