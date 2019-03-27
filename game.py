2154f1efe8d3604f4daab090ce2b9bd829083bc9
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-03-27 01:49:55 -0400

    Update item location printing func

--- game.py
+++ game.py
@@ -30,36 +30,75 @@ class Game:
   def print_item_locations(self):
     output = []
     for area in self.areas:
-      #print("Area %02X:" % area.area_index)
+      area_index = area.area_index
+      #print("Area %02X:" % area_index)
       for room in area.rooms:
         if room is None:
           continue
-        #print("On room %02X-%02X:" % (area.area_index, room.room_index))
+        room_index = room.room_index
+        #print("On room %02X-%02X:" % (area_index, room_index))
         
         for tile_entity in room.tile_entities:
           if tile_entity.type in [2, 3]:
             # Small chest or big chest
-            item_name = ITEM_ID_TO_NAME[tile_entity.item_id]
-            output.append("Room %02X-%02X:   Chest at %08X gives item %02X (%02X) - %s" % (area.area_index, room.room_index, tile_entity.entity_ptr, tile_entity.item_id, tile_entity.unknown_2, item_name))
+            item_id = tile_entity.item_id
+            item_name = ITEM_ID_TO_NAME[item_id]
+            item_param = tile_entity.unknown_2
+            output.append(
+              "Room %02X-%02X:   Chest at %08X gives item %02X (%02X) - %s" % (
+                area_index, room_index, tile_entity.entity_ptr, item_id, item_param, item_name
+              ))
         
         for entity_list in room.entity_lists:
           for entity in entity_list.entities:
-            if entity.type == 7 or (entity.type == 6 and entity.subtype in [0x69]):
+            if entity.type == 6 and entity.subtype in [0, 2, 5, 0x40, 0xAC]:
+              item_id = entity.form
+              if item_id == 0 and entity.subtype == 5:
+                # For pots, item ID 0 means random item
+                continue
+              item_name = ITEM_ID_TO_NAME[item_id]
+              if entity.subtype in [0, 2]:
+                item_param = entity.unknown_4
+              elif entity.subtype == 5:
+                item_param = entity.unknown_5
+              else:
+                item_param = 0
+              output.append(
+                "Room %02X-%02X:  Entity at %08X gives item %02X (%02X) - %s" % (
+                  area_index, room_index, entity.entity_ptr, item_id, item_param, item_name
+                ))
+        
+        # Items given by cutscenes
+        all_cutscenes_for_room = []
+        for entity_list in room.entity_lists:
+          for entity in entity_list.entities:
+            if entity.unknown_3 == 4 or entity.type == 7:
               if entity.params != 0:
                 cutscene = Cutscene(entity.params, self.rom)
-                for command in cutscene.commands:
-                  if command.type == 0x82:
-                    item_id = command.arguments[0]
-                    item_name = ITEM_ID_TO_NAME[item_id]
-                    output.append("Room %02X-%02X: Command at %08X gives item %02X (??) - %s" % (area.area_index, room.room_index, command.command_ptr, item_id, item_name))
-              if entity.subtype == 3: # Minish
-                cutscene_ptr = self.rom.read_u32(0x08109D18 + entity.unknown_4*4)
-                cutscene = Cutscene(cutscene_ptr, self.rom)
-                for command in cutscene.commands:
-                  if command.type == 0x82:
-                    item_id = command.arguments[0]
-                    item_name = ITEM_ID_TO_NAME[item_id]
-                    output.append("Room %02X-%02X: Command at %08X gives item %02X (??) - %s" % (area.area_index, room.room_index, command.command_ptr, item_id, item_name))
+                all_cutscenes_for_room.append(cutscene)
+            if entity.type == 7 and entity.subtype == 3: # Minish
+              cutscene_ptr = self.rom.read_u32(0x08109D18 + entity.unknown_4*4)
+              cutscene = Cutscene(cutscene_ptr, self.rom)
+              all_cutscenes_for_room.append(cutscene)
+        
+        for cutscene in all_cutscenes_for_room:
+          for command in cutscene.commands:
+            if command.type in [0x82, 0x85]:
+              item_id = command.arguments[0]
+              if item_id == 0x3F and len(command.arguments) > 1:
+                item_param = command.arguments[1]
+              else:
+                item_param = 0
+            elif command.type == 0x83:
+              item_id = 0x5C
+              item_param = command.arguments[0]
+            else:
+              continue
+            item_name = ITEM_ID_TO_NAME[item_id]
+            output.append(
+              "Room %02X-%02X: Command at %08X gives item %02X (%02X) - %s" % (
+                area_index, room_index, command.command_ptr, item_id, item_param, item_name
+              ))
     
     with open("item_locations.txt", "w") as f:
       for line in output:
